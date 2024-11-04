11ee7d5582e444e1b0a274a9e7d241d624acdeb6
Fabio Baptista <24209013+fabiobap@users.noreply.github.com>
2024-11-04 17:40:50 -0300

    response fix to handdle status no content

--- internal/pkg/helpers/responses.go
+++ internal/pkg/helpers/responses.go
@@ -8,6 +8,11 @@ import (
 func WriteResponse(w http.ResponseWriter, code int, data interface{}) {
 	w.Header().Add("Content-Type", "application/json")
 	w.WriteHeader(code)
+
+	if code == http.StatusNoContent {
+		return
+	}
+
 	if err := json.NewEncoder(w).Encode(data); err != nil {
 		panic(err)
 	}
